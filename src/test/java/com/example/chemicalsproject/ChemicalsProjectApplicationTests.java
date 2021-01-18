922e98c50a8e6e718437bd0400896894b1c36893
mft <mft@email.com>
2021-01-18 11:39:05 +0800

    Initial commit

---
+++ src/test/java/com/example/chemicalsproject/ChemicalsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chemicalsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemicalsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
