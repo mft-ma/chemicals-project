535d1a93fe3ca93ba13435b357242b7cfbc4a32c
cuihg <1747329347@qq.com>
2021-03-05 10:21:53 +0800

    提交发票管理

--- src/main/java/com/example/chemicalsproject/pojo/purchase.java
+++ src/main/java/com/example/chemicalsproject/pojo/purchase.java
@@ -1,30 +1,35 @@
 package com.example.chemicalsproject.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @TableName(value = "purchase")
-public class purchase {
+public class Purchase {
     @TableId(value = "cid",type = IdType.AUTO)
-    private Integer cid;  //主键
-    private Integer user_id;  //业务员id
-    private String name;  //产品名称
-    private String cas;  //cas号
-    private Integer amount;  //数量
-    private double price;  //单价
-    private String price_status;  //0  单价（含税）、1  单价（不含税）
-    private double sum_price;  //总价
-    private Integer status;  //收货状态  0未收货   1确认收货
-    private String supplier_name;  //供应商姓名
-    private String supplier_phone;  //供应商电话
-    private String tracking_number;  //物流单号
-    private Data create_time;  //创建日期
-    private Integer ann;  //1未收到发票，2收到发票
+    private int cid;
+    private int userId;
+    private String name;
+    private String cas;
+    private int amount;
+    private double price;
+    private String priceStatus;
+    private double sumPrice;
+    private String status;
+    private String supplierName;
+    private String supplierPhone;
+    private String trackingNumber;
+    private Date createTime;
+    private int ann;
+    @TableField(exist = false)
+    private User user;
 }
--- src/main/java/com/example/chemicalsproject/pojo/supplier.java
+++ src/main/java/com/example/chemicalsproject/pojo/supplier.java
@@ -1,27 +1,31 @@
 package com.example.chemicalsproject.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.xml.soap.Text;
 import java.sql.Date;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @TableName(value = "supplier")
-public class supplier {
+public class Supplier {
     @TableId(value = "gid",type = IdType.AUTO)
     private Integer gid;
-    private Integer user_id;
-    private String name;
+    private Integer userId;
+    private String name;//供应商名称
     private String phone;
     private String wechat;
     private String status;
-    private Date create_time;
-    private Text remark;
+    private Date createTime;
+    private String remark;//备注
+    @TableField(exist = false)
+    private User user;
+    @TableField(exist = false)
+    private String formatId;
 }
